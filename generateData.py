7a3c3f70952eb90ad6632e54b6a8067d3b39da39
Spacener <spencer.stepanek@colorado.edu>
2022-01-25 11:26:06 -0700

    imgGen

--- generateData.py
+++ generateData.py
@@ -29,27 +29,31 @@ valuable_block_names = [
 ]
 # n_to_generate = input("How many training images would you like to generate?: ")
 
+# initialize
+valueablePath = "textures/valuableBlocks/diamond_ore.png"
+path = "textures/commonBlocks/stone.png"
+
 # randomly select for deepslate or stone
 if np.random.random() >= 0.5: # deepslate coloration!
     path = "textures/commonBlocks/cobbled_deepslate.png"
-    bgColor = (90, 90, 90)
+    bgColor = (70, 70, 70)
 
     # randomly select a valuable block to include
     rand = np.random.random()
-    if rand > 1/7:
+    if rand < 1/7:
         valueablePath = "textures/valuableBlocks/deepslate_diamond_ore.png"
-    elif rand > 2/7:
+    elif rand < 2/7:
         valueablePath = "textures/valuableBlocks/deepslate_gold_ore.png"
-    elif rand > 3/7:
+    elif rand < 3/7:
         valueablePath = "textures/valuableBlocks/deepslate_lapis_ore.png"
-    elif rand > 4/7:
+    elif rand < 4/7:
         valueablePath = "textures/valuableBlocks/deepslate_iron_ore.png"
-    elif rand > 5/7:
+    elif rand < 5/7:
         valueablePath = "textures/valuableBlocks/deepslate_coal_ore.png"
-    elif rand > 6/7:
+    elif rand < 6/7:
         valueablePath = "textures/valuableBlocks/deepslate_redstone_ore.png"
-    elif rand > 1:
-        valueablePath = "textures/valuableBlocks/deepslate_emrald_ore.png"
+    elif rand < 1:
+        valueablePath = "textures/valuableBlocks/deepslate_emerald_ore.png"
 
 else: # Stone coloration!
     path = "textures/commonBlocks/stone.png"
@@ -57,50 +61,92 @@ else: # Stone coloration!
 
     # randomly select a valuable block to include
     rand = np.random.random()
-    if rand > 1/7:
+    if rand < 1/7:
         valueablePath = "textures/valuableBlocks/diamond_ore.png"
-    elif rand > 2/7:
+    elif rand < 2/7:
         valueablePath = "textures/valuableBlocks/gold_ore.png"
-    elif rand > 3/7:
+    elif rand < 3/7:
         valueablePath = "textures/valuableBlocks/lapis_ore.png"
-    elif rand > 4/7:
+    elif rand < 4/7:
         valueablePath = "textures/valuableBlocks/iron_ore.png"
-    elif rand > 5/7:
+    elif rand < 5/7:
         valueablePath = "textures/valuableBlocks/coal_ore.png"
-    elif rand > 6/7:
+    elif rand < 6/7:
         valueablePath = "textures/valuableBlocks/redstone_ore.png"
-    elif rand > 1:
-        valueablePath = "textures/valuableBlocks/emrald_ore.png"
+    elif rand < 1:
+        valueablePath = "textures/valuableBlocks/emerald_ore.png"
 
 
-img = cv2.imread(path)
+backgroundBlock = cv2.imread(path)
+valuableBlock = cv2.imread(valueablePath)
 
-inputImage = np.tile(img, (10,20,1)) # tile the image
+# Each corncer is a cell of 4 blocks
+# Four corners make a chunk of 16 square blocks
+
+# make a valuable corner!
+if np.random.random() <= 0.5:
+    valueCorner = cv2.vconcat([
+        cv2.hconcat([backgroundBlock, backgroundBlock]),
+        cv2.hconcat([backgroundBlock, valuableBlock])
+    ])
+else:
+    valueCorner = cv2.vconcat([
+        cv2.hconcat([backgroundBlock, valuableBlock]),
+        cv2.hconcat([valuableBlock, valuableBlock])
+    ])
+
+# assign a dull corner, with no valuable blocks
+dullCorner = cv2.vconcat([
+        cv2.hconcat([backgroundBlock, backgroundBlock]),
+        cv2.hconcat([backgroundBlock, backgroundBlock])
+    ])
+
+# 16 blocks
+dullChunk = cv2.vconcat([
+        cv2.hconcat([dullCorner, dullCorner]),
+        cv2.hconcat([dullCorner, dullCorner])
+    ])
+
+# valuable chunk
+chunk = cv2.vconcat([
+        cv2.hconcat([dullCorner, dullCorner]),
+        cv2.hconcat([dullCorner, valueCorner])
+    ])
+
+img = cv2.vconcat([
+        cv2.hconcat([dullChunk, dullChunk]),
+        cv2.hconcat([dullChunk, chunk])
+    ])
+
+cv2.imshow("chunk", img)
+tiled = np.tile(img, (2,2,1)) # tile the image
+
+outputImage = tiled.copy()
 
 # perspective transform
 # specify desired output size
 
-previousWidth = inputImage.shape[1]
-previousHeight = inputImage.shape[0]
-
-width = inputImage.shape[1]
-
-perspectiveView = False
+previousWidth = tiled.shape[1]
+previousHeight = tiled.shape[0]
 
 scale_percent = 2000
-ogWidth = int(img.shape[1] * scale_percent / 100)
-ogHeight = int(img.shape[0] * scale_percent / 100)
+ogWidth = int(backgroundBlock.shape[1] * scale_percent / 100)
+ogHeight = int(backgroundBlock.shape[0] * scale_percent / 100)
 
-if np.random.random() >= 0.5: # randomly select perspective view or flat
-    leftHeight = inputImage.shape[0]
-    rightHeight = inputImage.shape[0]
-else: # randomly select left or right perspective
+width = tiled.shape[1]
+
+# randomly select perspective view or flat
+if np.random.random() >= 0.5:
+    leftHeight = tiled.shape[0]
+    rightHeight = tiled.shape[0]
+else:
+    # randomly select left or right perspective
     if np.random.random() >= 0.5:
-        leftHeight = 2*inputImage.shape[0]/3
-        rightHeight = inputImage.shape[0]*1.5
+        leftHeight = 2*tiled.shape[0]/3
+        rightHeight = tiled.shape[0]*1.5
     else:
-        rightHeight = 2*inputImage.shape[0]/3
-        leftHeight = inputImage.shape[0]*1.5
+        rightHeight = 2*tiled.shape[0]/3
+        leftHeight = tiled.shape[0]*1.5
 
     # specify conjugate x,y coordinates (not y,x)
     input = np.float32([[0,0], [previousWidth,0], [previousWidth,previousHeight], [0,previousHeight]])
@@ -110,35 +156,33 @@ else: # randomly select left or right perspective
     matrix = cv2.getPerspectiveTransform(input,output)
 
     # do perspective transformation setting area outside input to black
-    inputImage = cv2.warpPerspective(inputImage,
+    perspective = cv2.warpPerspective(tiled, # named tiled, but it's actually perspective
                                      matrix,
                                      (ogWidth,ogHeight),
                                      cv2.INTER_LINEAR,
                                      borderMode=cv2.BORDER_CONSTANT,
                                      borderValue=bgColor)
 
-# Crop image
-# Save the lesser height value
-height = rightHeight
-if leftHeight < rightHeight: height = leftHeight
-if rightHeight < leftHeight: height = rightHeight
-else: height = rightHeight
+    outputImage = perspective.copy()
 
 # crop the image randomly
-cropped_image = inputImage[np.random.randint(0,int(height-100)):int(height), np.random.randint(0,int(width-100)):int(width)]
+newWidth = outputImage.shape[1]
+newHeight = outputImage.shape[0]
 
+cropped_image = outputImage[
+                np.random.randint(0,int(newHeight*.5)):int(newHeight*0.6),
+                np.random.randint(0,int(newWidth*.5)):int(newWidth*0.6)
+                ]
 
 # scale the image
-
-scale_percent = np.random.randint(3000, 4000)
-newWidth = int(img.shape[1] * scale_percent / 100)
-newHeight = int(img.shape[0] * scale_percent / 100)
+scale_percent = np.random.randint(180, 200)
+newWidth = int(newWidth * scale_percent / 100)
+newHeight = int(newHeight * scale_percent / 100)
 dim = (newWidth, newHeight)
 resized = cv2.resize(cropped_image, dim, interpolation=cv2.INTER_AREA)
 
-
 print("[DEBUG]: IMAGE DISPLAYED. PRESS ANY KEY TO ESCAPE")
-# cv2.imshow("perspective", inputImage)
+cv2.imshow("tiled", tiled)
 # cv2.imshow("cropped_image", cropped_image)
 cv2.imshow("resized", resized)
 
